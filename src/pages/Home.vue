d766563d6ed41d10515b60035caabc8f728ac4fa
Hothaifa Jaber <i74ifa@gmail.com>
2023-08-07 20:48:59 +0300

    add login page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,11 @@
   <div class="bg-[url('/maskgroup.png')] bg-left bg-no-repeat h-screen">
     <div class="max-w-screen-xl mx-auto">
       <div class="grid md:grid-cols-2 pt-40 items-center mb-20 md:px-2 px-5">
-        <div class=" text-xl md:text-2xl max-w-2xl text-gray-700 order-2 md:order-1 text-center md:text-inherit ">
+        <div
+          class="text-xl md:text-2xl max-w-2xl text-gray-700 order-2 md:order-1 text-center md:text-inherit"
+        >
           <p>منصة أُسس لتمويل نقاط البيع</p>
-          <p class="mb-3 ">
+          <p class="mb-3">
             منصة تعميد تساعد بدورها عمل وتعزيز شركات المعلومات الائتمانية و
             التصنيف الائتماني والمراجعة والتدقيق المالي التي سوف تحسن من قدرة
             تقييم المخاطر للشركات الصغيرة والمتوسطة في المملكة
@@ -24,18 +26,20 @@
       </div>
       <div class="mb-40">
         <h1 class="text-center text-3xl mb-4">شركاؤنا</h1>
-        <div class="py-20 bg-white/30 rounded-lg flex justify-around flex-wrap gap-y-10 md:gap-y-2">
+        <div
+          class="py-20 bg-white/30 rounded-lg flex justify-around flex-wrap gap-y-10 md:gap-y-2"
+        >
           <div>
-            <img src="/alrajhi.png" class="w-40" alt="">
+            <img src="/alrajhi.png" class="w-40" alt="" />
           </div>
           <div>
-            <img src="/riyad.png" class="w-40" alt="">
+            <img src="/riyad.png" class="w-40" alt="" />
           </div>
           <div>
-            <img src="/alinma-bank.png" class="w-40" alt="">
+            <img src="/alinma-bank.png" class="w-40" alt="" />
           </div>
           <div>
-            <img src="/anb-bank.png" class="w-36" alt="">
+            <img src="/anb-bank.png" class="w-36" alt="" />
           </div>
         </div>
       </div>
@@ -43,19 +47,27 @@
         <h1 class="text-center text-3xl mb-4">الجهات</h1>
         <div class="py-20 bg-white/30 rounded-lg flex justify-around flex-wrap">
           <div>
-            <img src="/Group.png" class="w-40" alt="">
+            <img src="/Group.png" class="w-40" alt="" />
           </div>
           <div>
-            <img src="/Group2.png" class="w-40" alt="">
+            <img src="/Group2.png" class="w-40" alt="" />
           </div>
           <div>
-            <img src="/Group3.png" class="w-40" alt="">
+            <img src="/Group3.png" class="w-40" alt="" />
           </div>
           <div>
-            <img src="/Group4.png" class="w-40" alt="">
+            <img src="/Group4.png" class="w-40" alt="" />
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+body {
+  background-color: #f5f5f5;
+  background: url(../assets/bbblurry.svg);
+  @apply bg-no-repeat bg-cover;
+}
+</style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,65 @@
 <template>
-    <div class="">
-        GG
-    </div>
+  <div class="">
+    <section class="text-gray-600 body-font">
+      <div
+        class="max-w-screen-xl mx-auto px-5 pt-44 pb-10 md:flex-row flex-col"
+      >
+        <div
+          class="flex flex-col md:items-start md:text-left md:mb-16 items-center text-center"
+        >
+          <h1
+            class="title-font md:text-4xl text-2xl font-medium text-gray-700 mb-16"
+          >
+            مرحباً بعودتك
+          </h1>
+        </div>
+        <div
+          class="flex justify-between items-center py-5 md:flex-row flex-col"
+        >
+          <h1 class="title-big text-center md:text-inherit">
+            سجّل الدخول<br />
+            إلى منصة أسس
+          </h1>
+          <div class="w-96">
+            <div>
+              <form action="">
+                <div class="relative">
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    class="input-default mb-6"
+                    placeholder="إسم المستخدم"
+                  />
+                </div>
+
+                <div class="relative">
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    class="input-default"
+                    placeholder="كلمة المرور"
+                  />
+                </div>
+                <button type="button" class="mb-6">
+                  <small class="text-gray-500">إستعادة كلمة المرور</small>
+                </button>
+                <button
+                  type="submit"
+                  class="py-2 px-4 bg-[#43A09D] hover:bg-[#36807d] focus:ring-gray-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg w-full"
+                >
+                  دخول
+                </button>
+              </form>
+              <div class="py-3">
+                إذا لم يكن لديك حساب ، يمكنك
+                <a href="/register" class="text-blue-400">إنشاء حساب</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
